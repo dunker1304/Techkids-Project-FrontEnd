84b4fcbb3c9bf303ed171e2be07e26a2d2bf7ad5
dunker <dunker1304@gmail.com>
2018-10-31 15:54:25 +0700

    Updated 31/10

--- src/Components/Comment.js
+++ src/Components/Comment.js
@@ -1,9 +1,24 @@
-import React, { Component } from 'react'
-import '../CSS/Comment.css'
-import iconBoy from '../images/boy.svg'
+import React, { Component } from 'react';
+import '../CSS/Comment.css';
+import iconBoy from '../images/boy.svg';
+import axios from 'axios';
 export default class Comment extends Component {
+  state={
+
+  }
+  componentDidMount(){
+    axios.get(`https://psychologist-backend.herokuapp.com/api/user/comment?userId=${this.props.info.author}`)
+        .then(data=>{
+            this.setState({user: data.data,
+            });
+           
+        })
+        .catch(error=>{
+            console.log(error);
+        });
+  }
   render() {
-    console.log(this.props.info);
+    console.log(this.state.user);
     return (
       
       <div className="row mt-3 comment">                        
@@ -13,7 +28,7 @@ export default class Comment extends Component {
           
           <div class="col-xs-3 col-sm-3 col-md-2 col-lg-2 justify-content-center  infoPost">
               <img className="text-center" src={iconBoy} alt="ava"/> <br/>
-              <span>By author: </span> <a href="/" className="author text-center">{this.props.info.author.username}</a> <br/>
+              <span>By author: </span> <a href="/" className="author text-center">{this.state.user.username}</a> <br/>
               <span>Upload: </span> <span className="time text-center detailpost">{this.props.info.createdAt.substring(0,10)}</span>
           </div>
       </div>
