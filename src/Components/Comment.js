0f725a5c27e10c557a468760ad544b99361a8262
dunker <dunker1304@gmail.com>
2018-10-31 15:58:22 +0700

    Updated 31/10

--- src/Components/Comment.js
+++ src/Components/Comment.js
@@ -28,7 +28,7 @@ export default class Comment extends Component {
           
           <div class="col-xs-3 col-sm-3 col-md-2 col-lg-2 justify-content-center  infoPost">
               <img className="text-center" src={iconBoy} alt="ava"/> <br/>
-              <span>By author: </span> <a href="/" className="author text-center">{this.state.user.username}</a> <br/>
+              <span>By author: </span> <a href="/" className="author text-center">{this.props.info.author}</a> <br/>
               <span>Upload: </span> <span className="time text-center detailpost">{this.props.info.createdAt.substring(0,10)}</span>
           </div>
       </div>
